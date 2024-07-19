511caf8f75ef9e3696c22162686160765be274b6
rkm0078 <rishabhm0078@gmail.com>
2024-07-19 13:15:26 +0530

    chnge

--- main.py
+++ main.py
@@ -52,12 +52,6 @@ async def on_message(message: Message) -> None:
     print(f'[{channel}] {username}: "{user_message}"')
     await send_message(message, user_message)
 
-@client.event
-async def on_message(message):
-    if message.content == '!privacy':
-        await message.channel.send('You can view our privacy policy here: [https://github.com/rkm0078/Discord-bot/blob/main/privacy_policy.md]')
-
-
 # Step 5: Main entry point
 def main() -> None:
     client.run(TOKEN)
